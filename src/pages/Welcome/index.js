b887475ff5b77b13e98a4eae72ccc9e4a1582877
dtambara <danilo.tambara@al.forsoft.org.br>
2018-09-02 22:31:48 -0300

    Estilização da página principal

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,11 +1,39 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+
+import styles from './styles';
 
 const Welcome = () => (
-  <View>
-    <Text>Welcome</Text>
+  <View style={styles.container}>
+    <Text style={styles.title}>Bem-Vindo</Text>
+    <Text style={styles.text}>
+      Para continuar, precisamos que você insira seu usuário no github.
+    </Text>
+
+    <View style={styles.form}>
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        style={styles.input}
+        placeholder="Digite seu usuário"
+        underlineColorAndroid="rgba(0,0,0,0)"
+      />
+
+      <TouchableOpacity style={styles.button} onPress={() => {}}>
+        <Text style={styles.buttonText}>Prosseguir</Text>
+      </TouchableOpacity>
+    </View>
   </View>
 );
+
+Welcome.navigationOptions = {
+  header: null,
+};
 
 
 export default Welcome;
