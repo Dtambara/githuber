9e0d708c090b1aaf4911bcb88d8729d44ed525a4
dtambara <danilo.tambara@al.forsoft.org.br>
2018-09-02 23:16:09 -0300

    Configurando navegação a partir da página principal

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,39 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { StackActions, NavigationActions } from 'react-navigation';
+import PropTypes from 'prop-types';
+
 import {
   View,
   Text,
   TextInput,
   TouchableOpacity,
+  StatusBar,
 } from 'react-native';
 
 import styles from './styles';
 
-const Welcome = () => (
-  <View style={styles.container}>
-    <Text style={styles.title}>Bem-Vindo</Text>
-    <Text style={styles.text}>
-      Para continuar, precisamos que você insira seu usuário no github.
-    </Text>
+export default class Welcome extends Component {
+  static navigationOptions = {
+    header: null,
+  };
 
-    <View style={styles.form}>
-      <TextInput
-        autoCapitalize="none"
-        autoCorrect={false}
-        style={styles.input}
-        placeholder="Digite seu usuário"
-        underlineColorAndroid="rgba(0,0,0,0)"
-      />
+  state = {};
 
-      <TouchableOpacity style={styles.button} onPress={() => {}}>
-        <Text style={styles.buttonText}>Prosseguir</Text>
-      </TouchableOpacity>
+  static propTypes = {
+    navigation: PropTypes.shape({
+      dispatch: PropTypes.func,
+    }).isRequired,
+  };
+
+  signin = () => {
+    const { navigation } = this.props;
+    const resetAction = StackActions.reset({
+      index: 0,
+      actions: [
+        NavigationActions.navigate({ routeName: 'User' }),
+      ],
+    });
+
+    navigation.dispatch(resetAction);
+  };
+
+  render = () => (
+    <View style={styles.container}>
+      <StatusBar barStyle="light-content" />
+
+      <Text style={styles.title}>Bem-Vindo</Text>
+      <Text style={styles.text}>
+        Para continuar, precisamos que você insira seu usuário no github.
+      </Text>
+
+      <View style={styles.form}>
+        <TextInput
+          autoCapitalize="none"
+          autoCorrect={false}
+          style={styles.input}
+          placeholder="Digite seu usuário"
+          underlineColorAndroid="rgba(0,0,0,0)"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={this.signin}>
+          <Text style={styles.buttonText}>Prosseguir</Text>
+        </TouchableOpacity>
+      </View>
     </View>
-  </View>
-);
-
-Welcome.navigationOptions = {
-  header: null,
-};
-
-
-export default Welcome;
+  );
+}
